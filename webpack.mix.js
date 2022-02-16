d40e30db794508d3d2a58af32fad3c51b663eb63
Nickolas A. Bini <nickolas.bini@crcpr.org.br>
2022-02-16 16:26:12 -0300

    Adding Bootstrap and Jquery to application

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,14 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix
+
+    .sass('node_modules/bootstrap/scss/bootstrap.scss', 'public/externalfeatures/bootstrap.css')
+
+    .scripts('node_modules/jquery/dist/jquery.js', 'public/externalfeatures/jquery.js')
+    .scripts('node_modules/bootstrap/dist/js/bootstrap.bundle.js', 'public/externalfeatures/bootstrap.js');
+
+    mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
