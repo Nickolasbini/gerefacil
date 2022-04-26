b72cf9fb82ef90dad990b19e45f20e23a8accc6e
Nickolas A. Bini <nickolas.bini@crcpr.org.br>
2022-04-26 17:23:35 -0300

    Typo

--- app/Http/Controllers/ProductOrderController.php
+++ app/Http/Controllers/ProductOrderController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\Product;
 use App\Models\ProductOrder;
+use Carbon\Carbon;
 
 class ProductOrderController extends Controller
 {
